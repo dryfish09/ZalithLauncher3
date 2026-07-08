2064cada211173a847c9677c04894a8fe06557c5
Star1xr <166748405+Star1xr@users.noreply.github.com>
2026-07-08 23:50:11 +0300

    A lil fix

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/BuiltInFileManager.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/BuiltInFileManager.kt
@@ -22,6 +22,7 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
